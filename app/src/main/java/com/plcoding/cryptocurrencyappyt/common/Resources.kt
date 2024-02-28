e3556caaacc1d67fefc87e538bdbb8b88c2e3c24
ShawSensei <shawkatwasit106@gmail.com>
2024-02-28 17:15:02 +0600

    update

--- app/src/main/java/com/plcoding/cryptocurrencyappyt/common/Resources.kt
+++
@@ -1,7 +0,0 @@
-package com.plcoding.cryptocurrencyappyt.common
-
-sealed class Resources<T>(val data: T? = null, val message: String? = null){
-    class Success<T>(data: T) : Resources<T>(data)
-    class Error<T>(message: String, data: T? = null) : Resources<T>(data,message)
-    class Loading<T>(data: T? = null) : Resources<T>(data)
-}
